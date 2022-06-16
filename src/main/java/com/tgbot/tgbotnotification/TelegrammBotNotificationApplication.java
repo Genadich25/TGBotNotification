d535b60b2d3148aff4ef1a7cfd006b736c7ad020
pin20 <pin200048@gmail.com>
2022-06-16 20:23:36 +0400

    Initial commit

---
+++ src/main/java/com/tgbot/tgbotnotification/TelegrammBotNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.tgbot.tgbotnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrammBotNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegrammBotNotificationApplication.class, args);
+    }
+
+}
