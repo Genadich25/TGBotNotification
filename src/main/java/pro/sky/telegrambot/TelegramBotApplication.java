bf8098cd9127ccdf51e748c9684c24c744fe946b
pin20 <pin200048@gmail.com>
2022-06-23 18:02:50 +0400

    1.2 push

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -1,4 +1,4 @@
-package com.tgbot.tgbotnotification;
+package pro.sky.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
